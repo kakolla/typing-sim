e8a7a24d3664e759c5b43d41442b711cf45fc364
kakolla <abhishekkakolla1@gmail.com>
2026-06-13 01:53:43 -0700

    get macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Type")]
+async fn main() {
+    let game_done: bool = false;
+    while !game_done {
+        clear_background(BLACK);
+
+        draw_line(40.0, 40.0, 100.0, 150.0, 60.3, BLUE);
+        draw_text("welcome", 300.0, 800.0, 16.0, WHITE);
+        next_frame().await;
+    }
 }
